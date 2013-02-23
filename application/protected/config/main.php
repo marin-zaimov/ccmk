afb22c7d44c919f57994f02a8c6b122e65b0a3bb
Karol Chudy <chudy@gatech.edu>
2013-02-23 00:45:46 -0500

    Changed back to localhost

--- application/protected/config/main.php
+++ application/protected/config/main.php
@@ -56,7 +56,7 @@ return array(
 		// uncomment the following to use a MySQL database
 		
 		'db'=>array(
-			'connectionString' => 'mysql:host=127.0.0.1;dbname=ccmk',
+			'connectionString' => 'mysql:host=localhost;dbname=ccmk',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => 'marin',
