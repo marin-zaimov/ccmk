002556b2f8602db5115cd2ece61c0b573535dffa
Ubuntu <ubuntu@domU-12-31-39-16-2D-7C.compute-1.internal>
2013-02-23 07:51:23 +0000

    FIXED THE DB

--- application/protected/config/main.php
+++ application/protected/config/main.php
@@ -26,7 +26,7 @@ return array(
 			'class'=>'system.gii.GiiModule',
 			'password'=>'ccmk',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
+			'ipFilters'=>array('*','::1'),
 		),
 		
 	),
@@ -56,7 +56,7 @@ return array(
 		// uncomment the following to use a MySQL database
 		
 		'db'=>array(
-			'connectionString' => 'mysql:localhost;dbname=ccmk',
+			'connectionString' => 'mysql:host=127.0.0.1;port=3306;dbname=ccmk',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => 'marin',
