dead84c51c8ffbb7513944629edb8cf36f9baf4a
Karol Chudy <chudy@gatech.edu>
2013-02-23 01:23:59 -0500

    Re added connection info to jsonTest

--- application/jsonTest.php
+++ application/jsonTest.php
@@ -1,2 +1,15 @@
 <?php
-echo "hi"; ?>
+$connect = mysql_connect("localhost","root","marin");
+
+if (!$connect) {
+    die("Couldn't connect:" . mysql_error());
+}
+
+mysql_select_db("ccmk");
+
+$sql=mysql_query("select * from User");
+//echo $sql;
+while($row=mysql_fetch_assoc($sql)) $output[]=$row;
+print(json_encode($output));
+
+mysql_close(); ?>
