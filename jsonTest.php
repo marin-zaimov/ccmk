4571816ce21d23f459674f51c42ec59780a31a25
Karol Chudy <chudy@gatech.edu>
2013-02-23 01:15:30 -0500

    Added json test

---
+++ jsonTest.php
@@ -0,0 +1,15 @@
+<?php
+$connect = mysql_connect("localhost","root","marin");
+
+if (!$connect) {
+    die("Couldn't connect:" . mysql_error());
+}
+
+mysql_select_db("ccmk");
+
+$sql=mysql_query("select * from User");
+//echo $sql;
+while($row=mysql_fetch_assoc($sql)) $output[]=$row;
+print(json_encode($output));
+
+mysql_close(); ?>
