e21c9e2ca510fa72f03c911e4813ddd8d7bee5aa
Karol Chudy <chudy@gatech.edu>
2013-02-23 01:25:43 -0500

    phpinfo

--- application/jsonTest.php
+++ application/jsonTest.php
@@ -1,15 +1,16 @@
 <?php
-$connect = mysql_connect("localhost","root","marin");
+phpinfo();
+// $connect = mysql_connect("localhost","root","marin");
 
-if (!$connect) {
-    die("Couldn't connect:" . mysql_error());
-}
+// if (!$connect) {
+//     die("Couldn't connect:" . mysql_error());
+// }
 
-mysql_select_db("ccmk");
+// mysql_select_db("ccmk");
 
-$sql=mysql_query("select * from User");
-//echo $sql;
-while($row=mysql_fetch_assoc($sql)) $output[]=$row;
-print(json_encode($output));
+// $sql=mysql_query("select * from User");
+// //echo $sql;
+// while($row=mysql_fetch_assoc($sql)) $output[]=$row;
+// print(json_encode($output));
 
-mysql_close(); ?>
+// mysql_close(); ?>
